1fb39e86c4509a335721b22bd9dd3a56fe44d1c4
YusufAlvin <yusufalvin0799@gmail.com>
2022-03-06 09:13:37 +0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import './main.css';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+
+let rootElement = document.getElementById('root');
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
